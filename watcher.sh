6a015eab6e3c6fa716ac65edcb6a3abca1b438fd
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2024-04-21 00:10:44 +0000

    Monitoring and scalling down of swype-app deployment

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,17 @@
-#TODO
+#!/usr/bin/env bash
+
+namespace="sre"
+deployment="swype-app"
+maxRestarts=3
+timeout=20
+
+while [ true ]; do
+    restarts=`kubectl get pods -n $namespace | grep $deployment | grep -v grep | awk '{ print $4 }'`
+    echo "Current restarts count: $restarts"
+    if [ $restarts -gt $maxRestarts ]; then
+        echo "Maximum number of restarts reached. Scalling $namespace:$deployment down"
+        kubectl scale deployment -n $namespace $deployment --replicas=0
+        break
+    fi
+    sleep $timeout
+done
