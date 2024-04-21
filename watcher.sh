6ef1995d2f2e3114c19dced6f5a8df2155f28823
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2024-04-20 22:54:43 -0400

    More information added to the output

--- watcher.sh
+++ watcher.sh
@@ -3,15 +3,28 @@
 namespace="sre"
 deployment="swype-app"
 maxRestarts=3
-timeout=20
+timeout=30
+
+# check if the namespace exists
+if ! kubectl get namespace $namespace &> /dev/null; then
+  echo "Namespace $namespace does not exist"
+  exit 1
+fi
+
+echo "Montioring $namespace:$deployment for restarts"
 
 while [ true ]; do
-    restarts=`kubectl get pods -n $namespace | grep $deployment | grep -v grep | awk '{ print $4 }'`
-    echo "Current restarts count: $restarts"
-    if [ $restarts -gt $maxRestarts ]; then
-        echo "Maximum number of restarts reached. Scalling $namespace:$deployment down"
-        kubectl scale deployment -n $namespace $deployment --replicas=0
-        break
-    fi
-    sleep $timeout
+  pod=`kubectl get pods -n $namespace | grep -v grep | grep $deployment`
+  if [ -z "$pod" ]; then
+    echo "Deployment $namespace:$deployment not found"
+    exit 1
+  fi
+  restarts=`echo $pod | awk '{print $4}'`
+  echo "Current restarts count: $restarts"
+  if [ $restarts -gt $maxRestarts ]; then
+    echo "Maximum number of restarts reached. Scalling $namespace:$deployment down"
+    kubectl scale deployment -n $namespace $deployment --replicas=0
+    break
+  fi
+  sleep $timeout
 done
